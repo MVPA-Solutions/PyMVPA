f74050b1888f7ee7b14f857f6ffd3fcbf4ea465c
MVPA-Solutions <email@example.com>
2024-12-19 17:10:18 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
-# vi: set ft=python sts=4 ts=4 sw=4 et:
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-#
-#   See COPYING file distributed along with the PyMVPA package for the
-#   copyright and license terms.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
 """Python distutils setup for PyMVPA"""
 
 from numpy.distutils.core import setup, Extension
@@ -180,15 +172,15 @@ def setup_package():
     os.chdir(src_path)
     sys.path.insert(0, src_path)
 
-    setup(name='pymvpa2',
-          version='2.6.5.dev1',
-          author='Michael Hanke, Yaroslav Halchenko, Nikolaas N. Oosterhof',
-          author_email='pkg-exppsy-pymvpa@lists.alioth.debian.org',
+    setup(name='pymvpa',
+          version='3.0.0',
+          author='MVPA-Solutions',
+          author_email='your.email@example.com',
           license='MIT License',
           url='http://www.pymvpa.org',
           description='Multivariate pattern analysis',
           long_description=
-              "PyMVPA is a Python module intended to ease pattern classification "
+              "PyMVPA is an updated Python module intended to ease pattern classification "
               "analyses of large datasets. It provides high-level abstraction of "
               "typical processing steps and a number of implementations of some "
               "popular algorithms. While it is not limited to neuroimaging data "
